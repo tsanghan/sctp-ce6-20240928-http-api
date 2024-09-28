bcaa01e5f2289e5bbad6228df7481898ee86f030
Wong Tsang Han <tsanghan@gmail.com>
2024-09-28 13:04:19 +0800

    commit 12

--- send_requests.sh
+++ send_requests.sh
@@ -1,9 +1,10 @@
-#!/bin/bash
+#!/use/bin/env bash
 
-INVOKE_URL="https://uq5k4byzp8.execute-api.ap-southeast-1.amazonaws.com"
+#INVOKE_URL="https://uq5k4byzp8.execute-api.ap-southeast-1.amazonaws.com"
+INVOKE_URL="https://tsanghan-httpapi.sctp-sandbox.com"
 
 for i in $(seq 2001 2050); do
     json=$(jq -n --arg year "$i" --arg title "MovieTitle$i" '{year: $year, title: $title}')
-    curl --header "Content-Type: application/json" --data "$json" "$INVOKE_URL/movies"
+    curl --header "Content-Type: application/json" --data "$json" "$INVOKE_URL/movies"; echo
 done
 
