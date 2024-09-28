83587197ab7845e187de90108294b34cb67416a8
Wong Tsang Han <tsanghan@gmail.com>
2024-09-28 13:11:18 +0800

    commit 14

--- provider.tf
+++ provider.tf
@@ -1,11 +1,11 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.69.0"
     }
     archive = {
-      source = "hashicorp/archive"
+      source  = "hashicorp/archive"
       version = "2.6.0"
     }
   }
