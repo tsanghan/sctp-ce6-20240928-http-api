eece2ef212ab01e6567139b0d032d35799d20165
Wong Tsang Han <tsanghan@gmail.com>
2024-09-28 20:50:30 +0800

    commit 33

--- src/app.py
+++ src/app.py
@@ -14,15 +14,13 @@ def lambda_handler(event, context):
   logging.info(f"## Loaded table name from environemt variable DDB_TABLE: {table}")
   logging.info(f"## Payload format: {event.get("version")}")
   logging.info(f"## Base64Encoded: {event.get("isBase64Encoded")}")
-  logging.info(f"## Payload Body: {event.get("body")} -> {bool(event.get("body"))} -> {type(event.get("body"))}")
-  logging.info(f"## Payload Compare: event.get('body') {bool(event.get("body"))} : {bool({event.get("body")} != '{{}}')}")
   if event.get("body") and event.get("body") != "{}":
       item = json.loads(base64.b64decode(event['body']).decode('utf-8')
                         if event["isBase64Encoded"]
                         else event['body'])
       logging.info(f"## Received payload: {item}")
-      year = str(item["year"])
-      title = str(item["title"])
+      year = item.get("year")
+      title = item.get("title")
       dynamodb_client.put_item(TableName=table,Item={"year": {'N':year}, "title": {'S':title}})
       message = "Successfully inserted data!"
       return {
