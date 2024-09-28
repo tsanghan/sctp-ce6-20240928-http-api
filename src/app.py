fc92c530c9d67176a9c229581fd10c4e2261bfb8
Wong Tsang Han <tsanghan@gmail.com>
2024-09-28 19:01:13 +0800

    commit 22

--- src/app.py
+++ src/app.py
@@ -12,8 +12,9 @@ dynamodb_client = boto3.client('dynamodb')
 def lambda_handler(event, context):
   table = os.environ.get('DDB_TABLE')
   logging.info(f"## Loaded table name from environemt variable DDB_TABLE: {table}")
+  logging.info(f"## Payload format: {event["version"]}")
+  logging.info(f"## Base64Encoded: {event["isBase64Encoded"]}")
   if event.get("body") and event.get("body") != {}:
-      logging.info(f"## Payload format: {event["version"]}, ## Base64Encoded: {event["isBase64Encoded"]}")
       item = json.loads(base64.b64decode(event['body']).decode('utf-8')
                         if event["isBase64Encoded"]
                         else event['body'])
